6ad556d2f7beed07d2226e3b4c278cd58d2e5fc9
Kobayasi, Hiroaki <buribullet@gmail.com>
2024-08-16 17:07:56 +0900

    Provide SERVICE_NAME from gitlab-ci.yml

--- cloud-run.sh
+++ cloud-run.sh
@@ -56,8 +56,12 @@ GCP_CLOUD_RUN_CPU="${GCP_CLOUD_RUN_CPU:-1000m}"
 
 ## private variables
 
-# SERVICE_NAME="${SERVICE_NAME:-gitlab-$CI_PROJECT_ID-$CI_COMMIT_REF_SLUG}"
-SERVICE_NAME=yatt-yl-podview
+if [[ -z "$SERVICE_NAME" ]]; then
+  SERVICE_NAME="${SERVICE_NAME:-gitlab-$CI_PROJECT_ID-$CI_COMMIT_REF_SLUG}"
+  echo Generated SERVICE_NAME=$SERVICE_NAME
+else
+  echo Provided SERVICE_NAME=$SERVICE_NAME
+fi
 
 __GCP_SERVICE_ACCOUNT_KEY_PRIVATE_KEY_DATA_FILE_NAME=local-service-account-key-private-key-data.txt
 __GCP_SERVICE_ACCOUNT_KEY_FILE_NAME=local-service-account-key-file.json
